31a18b28cc8caf18084d3b394394c628a2efc77a
AdhamMoussa <adam.moussa62017@gmail.com>
2019-03-16 20:07:19 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    siteMetadata: {
+        description: `A React Developer with experience in JavaScript ecosystem.`,
+        image: `/images/logo.png`,
+        title: `Adham Moussa | Portfolio`,
+    },
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-layout`,
+        `gatsby-plugin-netlify-cms`
+    ]
+}
